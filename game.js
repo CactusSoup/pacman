672e96bb94d4d22d2cdd960f87807fc9eee1c760
Thomas Morgan <thomas.morgan@avtura.com>
2024-05-17 21:30:50 +0100

    Prettier formatting

--- game.js
+++ game.js
@@ -19,231 +19,269 @@ let lives = 3;
 let ghosts = [];
 let ghostCount = 4;
 
-let createRect = (x,y,width,height,color) => {
-    canvasCtx.fillStyle = color;
-    canvasCtx.fillRect(x,y,width,height);
-}
+let createRect = (x, y, width, height, color) => {
+  canvasCtx.fillStyle = color;
+  canvasCtx.fillRect(x, y, width, height);
+};
 
-let createBlock = (x,y,color) => {
-    createRect(x * blockSize,y * blockSize,blockSize,blockSize,color);
-}
+let createBlock = (x, y, color) => {
+  createRect(x * blockSize, y * blockSize, blockSize, blockSize, color);
+};
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_DOWN = 1;
 
-
 let ghostLocations = [
-    {x: 0, y: 0},
-    {x: 176, y: 0},
-    {x: 0, y: 121},
-    {x:176,  y:121}
-]
+  { x: 0, y: 0 },
+  { x: 176, y: 0 },
+  { x: 0, y: 121 },
+  { x: 176, y: 121 },
+];
 
 let map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
-    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
-    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
-    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
-    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
-    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+  [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+  [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
-    for (let index = 0; index < map.length; index++) {
-        for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++){
-            if (map[index][secondIndex] == 2) {
-                foodCount++;
-            }
-        }
+for (let index = 0; index < map.length; index++) {
+  for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++) {
+    if (map[index][secondIndex] == 2) {
+      foodCount++;
     }
+  }
+}
 
 let randomTargetsForGhosts = [
-    {x: 1 * blockSize, y: 1 * blockSize},
-    {x: 1 * blockSize, y: (map.length - 2) * blockSize},
-    {x: (map[0].length -2) * blockSize, y: blockSize},
-    {x: (map[0].length -2) * blockSize, y: (map.length -2) * blockSize}
-]
+  { x: 1 * blockSize, y: 1 * blockSize },
+  { x: 1 * blockSize, y: (map.length - 2) * blockSize },
+  { x: (map[0].length - 2) * blockSize, y: blockSize },
+  { x: (map[0].length - 2) * blockSize, y: (map.length - 2) * blockSize },
+];
 
 let gameLoop = () => {
-    draw();
-    update();
-}
+  draw();
+  update();
+};
 
 let update = () => {
-    pacman.move();
-    pacman.eat();
-    for (let index = 0; index < ghosts.length; index++){
-        ghosts[index].move();
-    }
-    if (pacman.checkGhostCollision()){
-        restartGame();
-    }
-    if (score >= foodCount) { 
-        drawWin();
-        clearInterval(gameInterval);
-    }
-}
+  pacman.move();
+  pacman.eat();
+  for (let index = 0; index < ghosts.length; index++) {
+    ghosts[index].move();
+  }
+  if (pacman.checkGhostCollision()) {
+    restartGame();
+  }
+  if (score >= foodCount) {
+    drawWin();
+    clearInterval(gameInterval);
+  }
+};
 
 let restartGame = () => {
-    createNewPacman();
-    createGhosts();
-    lives--;
-    if (lives == 0) {
-        gameOver();
-    }
-}
-
+  createNewPacman();
+  createGhosts();
+  lives--;
+  if (lives == 0) {
+    gameOver();
+  }
+};
 
 let gameOver = () => {
-    drawGameOver();
-    clearInterval(gameInterval);
-}
+  drawGameOver();
+  clearInterval(gameInterval);
+};
 
 let drawGameOver = () => {
-    canvasCtx.font = "20px Emulogic";
-    canvasCtx.fillStyle = "white";
-    canvasCtx.fillText("Game Over!", 150, 200);
-}
+  canvasCtx.font = "20px Emulogic";
+  canvasCtx.fillStyle = "white";
+  canvasCtx.fillText("Game Over!", 150, 200);
+};
 
 let drawWin = () => {
-    canvasCtx.font = "20px Emulogic";
-    canvasCtx.fillStyle = "white";
-    canvasCtx.fillText("Winner!", 150, 200);
-}
+  canvasCtx.font = "20px Emulogic";
+  canvasCtx.fillStyle = "white";
+  canvasCtx.fillText("Winner!", 150, 200);
+};
 
 let drawLives = () => {
-    canvasCtx.font = "20px Emulogic";
-    canvasCtx.fillStyle = "white";
-    canvasCtx.fillText("Lives: ", 260, blockSize * (map.length +1) + 10);
-    for (let index = 0; index < lives; index++) {
-        canvasCtx.drawImage(pacmanFrames, 2 * blockSize, 0, blockSize, blockSize, 350 + index * blockSize, blockSize * map.length + 10, blockSize, blockSize);
-    }
-}
+  canvasCtx.font = "20px Emulogic";
+  canvasCtx.fillStyle = "white";
+  canvasCtx.fillText("Lives: ", 260, blockSize * (map.length + 1) + 10);
+  for (let index = 0; index < lives; index++) {
+    canvasCtx.drawImage(
+      pacmanFrames,
+      2 * blockSize,
+      0,
+      blockSize,
+      blockSize,
+      350 + index * blockSize,
+      blockSize * map.length + 10,
+      blockSize,
+      blockSize,
+    );
+  }
+};
 
 let draw = () => {
-    createRect(0,0,canvas.width, canvas.height, "black");
-    drawWalls();
-    pacman.draw();
-    drawFood();
-    drawScore();
-    drawGhosts();
-    drawLives();
-}
+  createRect(0, 0, canvas.width, canvas.height, "black");
+  drawWalls();
+  pacman.draw();
+  drawFood();
+  drawScore();
+  drawGhosts();
+  drawLives();
+};
 
 let drawGhosts = () => {
-    for (let index = 0; index < ghosts.length; index++){
-        ghosts[index].draw();
-    }
-}
+  for (let index = 0; index < ghosts.length; index++) {
+    ghosts[index].draw();
+  }
+};
 
 let drawScore = () => {
-    canvasCtx.font = "20px Emulogic";
-    canvasCtx.fillStyle = "white";
-    canvasCtx.fillText( "Score: " + score, 0, blockSize * (map.length + 1) + 10);
-}
-let gameInterval = setInterval(gameLoop, 1000/ fps);
+  canvasCtx.font = "20px Emulogic";
+  canvasCtx.fillStyle = "white";
+  canvasCtx.fillText("Score: " + score, 0, blockSize * (map.length + 1) + 10);
+};
+let gameInterval = setInterval(gameLoop, 1000 / fps);
 
 let drawWalls = () => {
-    for (let index = 0; index < map.length; index++) {
-        for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++){
-            if (map[index][secondIndex] == 1) {
-                // Wall
-                createBlock(secondIndex, index, wallColour);
-                if (secondIndex > 0 && map[index][secondIndex -1] == 1){
-                    createRect(secondIndex * blockSize, index * blockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColour);
-                }
-                if (secondIndex < map[index].length - 1 && map[index][secondIndex + 1] == 1){
-                    createRect(secondIndex * blockSize + wallOffset, index * blockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColour);
-                }
-                if (index > 0 && map[index - 1][secondIndex] == 1){
-                    createRect(secondIndex * blockSize + wallOffset, index * blockSize  , wallSpaceWidth , wallSpaceWidth + wallOffset, wallInnerColour);
-                }
-                if (index < map.length - 1 && map[index + 1][secondIndex] == 1){
-                    createRect(secondIndex * blockSize + wallOffset, index * blockSize + wallOffset , wallSpaceWidth , wallSpaceWidth + wallOffset, wallInnerColour);
-                }
-            }
+  for (let index = 0; index < map.length; index++) {
+    for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++) {
+      if (map[index][secondIndex] == 1) {
+        // Wall
+        createBlock(secondIndex, index, wallColour);
+        if (secondIndex > 0 && map[index][secondIndex - 1] == 1) {
+          createRect(
+            secondIndex * blockSize,
+            index * blockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColour,
+          );
         }
+        if (
+          secondIndex < map[index].length - 1 &&
+          map[index][secondIndex + 1] == 1
+        ) {
+          createRect(
+            secondIndex * blockSize + wallOffset,
+            index * blockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColour,
+          );
+        }
+        if (index > 0 && map[index - 1][secondIndex] == 1) {
+          createRect(
+            secondIndex * blockSize + wallOffset,
+            index * blockSize,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColour,
+          );
+        }
+        if (index < map.length - 1 && map[index + 1][secondIndex] == 1) {
+          createRect(
+            secondIndex * blockSize + wallOffset,
+            index * blockSize + wallOffset,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColour,
+          );
+        }
+      }
     }
-}
+  }
+};
 
 let drawFood = () => {
-    for (let index = 0; index < map.length; index++) {
-        for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++){
-            if (map[index][secondIndex] == 2) {
-                // Food
-                createRect(secondIndex * blockSize + blockSize /3,index * blockSize + blockSize /3, blockSize/3, blockSize /3, foodColour);
-            }
-        }
+  for (let index = 0; index < map.length; index++) {
+    for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++) {
+      if (map[index][secondIndex] == 2) {
+        // Food
+        createRect(
+          secondIndex * blockSize + blockSize / 3,
+          index * blockSize + blockSize / 3,
+          blockSize / 3,
+          blockSize / 3,
+          foodColour,
+        );
+      }
     }
-}
+  }
+};
 
 let createNewPacman = () => {
-    pacman = new Pacman(
-        blockSize,
-        blockSize,
-        blockSize,
-        blockSize,
-        blockSize / 5,
-        DIRECTION_RIGHT
-    );
-}
+  pacman = new Pacman(
+    blockSize,
+    blockSize,
+    blockSize,
+    blockSize,
+    blockSize / 5,
+    DIRECTION_RIGHT,
+  );
+};
 
 let createGhosts = () => {
-    ghosts = []
-    for (let index = 0; index < ghostCount; index++){
-        let newGhost = new Ghost(
-            9 * blockSize + (index %2 == 0 ? 0 : 1) * blockSize,
-            10 * blockSize + (index %2 == 0 ? 0 : 1 ) * blockSize,
-            blockSize,
-            blockSize,
-            pacman.speed/2,
-            DIRECTION_RIGHT,
-            ghostLocations[index % 4].x,
-            ghostLocations[index % 4].y,
-            124, 116, 6 + index
-        );
-        ghosts.push(newGhost);
-    }
-
-}
+  ghosts = [];
+  for (let index = 0; index < ghostCount; index++) {
+    let newGhost = new Ghost(
+      9 * blockSize + (index % 2 == 0 ? 0 : 1) * blockSize,
+      10 * blockSize + (index % 2 == 0 ? 0 : 1) * blockSize,
+      blockSize,
+      blockSize,
+      pacman.speed / 2,
+      DIRECTION_RIGHT,
+      ghostLocations[index % 4].x,
+      ghostLocations[index % 4].y,
+      124,
+      116,
+      6 + index,
+    );
+    ghosts.push(newGhost);
+  }
+};
 
 createNewPacman();
 createGhosts();
 gameLoop();
 
 window.addEventListener("keydown", (event) => {
-    let k = event.keyCode
+  let k = event.keyCode;
 
-    setTimeout(() => {
-        if (k == 37 || k == 65){
-            pacman.nextDirection =DIRECTION_LEFT;
-        } else if (k == 38 || k == 87) {
-            pacman.nextDirection =DIRECTION_UP;
-
-        } else if (k == 39 || k == 68){
-            pacman.nextDirection =DIRECTION_RIGHT;
-
-        } else if (k == 40 || k == 83){
-            pacman.nextDirection =DIRECTION_DOWN;
-
-        }
-
-    }, 1)
-})
+  setTimeout(() => {
+    if (k == 37 || k == 65) {
+      pacman.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 87) {
+      pacman.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      pacman.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 83) {
+      pacman.nextDirection = DIRECTION_DOWN;
+    }
+  }, 1);
+});
--- pacman.js
+++ pacman.js
@@ -1,135 +1,157 @@
 class Pacman {
-    constructor(x,y,width,height,speed,direction){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-        this.direction = direction;
-        this.nextDirection = this.direction;
-        this.currentFrame = 1;
-        this.frameCount = 7;
+  constructor(x, y, width, height, speed, direction) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.direction = direction;
+    this.nextDirection = this.direction;
+    this.currentFrame = 1;
+    this.frameCount = 7;
 
-        setInterval(() => {
-            this.changeAnimation();
-        }, 100);
+    setInterval(() => {
+      this.changeAnimation();
+    }, 100);
+  }
+
+  move() {
+    this.changeDirectionIfPossible();
+    this.moveForwards();
+    if (this.checkCollision()) {
+      this.moveBackwards();
     }
+  }
 
-    move() {
-        this.changeDirectionIfPossible();
-        this.moveForwards();
-        if (this.checkCollision()){
-            this.moveBackwards();
-        }
-    }
-
-    eat() {
+  eat() {
     for (let index = 0; index < map.length; index++) {
-        for (let secondIndex = 0; secondIndex < map[index].length; secondIndex++){
-            if (map[index][secondIndex] == 2 && this.getMapX() == secondIndex && this.getMapY() == index) {
-                map[index][secondIndex] = 3;
-                    score+=1;
-            }
+      for (
+        let secondIndex = 0;
+        secondIndex < map[index].length;
+        secondIndex++
+      ) {
+        if (
+          map[index][secondIndex] == 2 &&
+          this.getMapX() == secondIndex &&
+          this.getMapY() == index
+        ) {
+          map[index][secondIndex] = 3;
+          score += 1;
         }
+      }
+    }
+  }
+
+  changeDirectionIfPossible() {
+    if (this.direction == this.nextDirection) {
+      return;
+    }
+    let tempDirection = this.direction;
+    this.direction = this.nextDirection;
+    this.moveForwards();
+    if (this.checkCollision()) {
+      this.moveBackwards();
+      this.direction = tempDirection;
+    } else {
+      this.moveBackwards();
+    }
+  }
+  moveBackwards() {
+    switch (this.direction) {
+      case DIRECTION_RIGHT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION_LEFT:
+        this.x += this.speed;
+        break;
+      case DIRECTION_UP:
+        this.y += this.speed;
+        break;
+      case DIRECTION_DOWN:
+        this.y -= this.speed;
+        break;
+    }
+  }
+  moveForwards() {
+    switch (this.direction) {
+      case DIRECTION_RIGHT:
+        this.x += this.speed;
+        break;
+      case DIRECTION_LEFT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION_UP:
+        this.y -= this.speed;
+        break;
+      case DIRECTION_DOWN:
+        this.y += this.speed;
+        break;
+    }
+  }
+
+  checkCollision() {
+    if (
+      map[this.getMapY()][this.getMapX()] == 1 ||
+      map[this.getMapYRightSide()][this.getMapX()] == 1 ||
+      map[this.getMapY()][this.getMapXRightSide()] == 1 ||
+      map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+    ) {
+      return true;
     }
 
-    }
+    return false;
+  }
 
-    changeDirectionIfPossible() {
-        if (this.direction == this.nextDirection){
-            return;
-        }
-        let tempDirection = this.direction
-        this.direction = this.nextDirection
-        this.moveForwards();
-        if (this.checkCollision()){
-            this.moveBackwards();
-            this.direction = tempDirection;
-        } else {
-            this.moveBackwards();
-        }
+  checkGhostCollision() {
+    for (let index = 0; index < ghosts.length; index++) {
+      let ghost = ghosts[index];
+      if (
+        ghost.getMapX() == this.getMapX() &&
+        ghost.getMapY() == this.getMapY()
+      ) {
+        return true;
+      }
+      return false;
+    }
+  }
 
-    }
-    moveBackwards() {
-        switch(this.direction){
-            case DIRECTION_RIGHT:
-                this.x -= this.speed;
-                break;
-            case DIRECTION_LEFT:
-                this.x += this.speed;
-                break;
-            case DIRECTION_UP:
-                this.y += this.speed;
-                break;
-            case DIRECTION_DOWN:
-                this.y -= this.speed;
-                break;
-        }
-    }
-    moveForwards() {
-        switch(this.direction){
-            case DIRECTION_RIGHT:
-                this.x += this.speed;
-                break;
-            case DIRECTION_LEFT:
-                this.x -= this.speed;
-                break;
-            case DIRECTION_UP:
-                this.y -= this.speed;
-                break;
-            case DIRECTION_DOWN:
-                this.y += this.speed;
-                break;
-        }
-    }
+  getMapX() {
+    return parseInt(this.x / blockSize);
+  }
 
-    checkCollision(){
-        if (map[this.getMapY()][this.getMapX()] == 1 ||
-        map[this.getMapYRightSide()][this.getMapX()] == 1 ||
-        map[this.getMapY()][this.getMapXRightSide()] == 1 ||
-        map[this.getMapYRightSide()][this.getMapXRightSide()] == 1){
-            return true;
-        }
+  getMapY() {
+    return parseInt(this.y / blockSize);
+  }
 
-        return false;
-    }
+  getMapXRightSide() {
+    return parseInt((this.x + 0.9999 * blockSize) / blockSize);
+  }
 
-    checkGhostCollision(){
-        for (let index = 0; index < ghosts.length; index++){
-            let ghost = ghosts[index];
-            if (ghost.getMapX() == this.getMapX() && ghost.getMapY() == this.getMapY()){
-                return true;
-            }
-            return false;
-        }
-    }
-    
-    getMapX(){
-        return parseInt(this.x / blockSize);
-    }
+  getMapYRightSide() {
+    return parseInt((this.y + 0.9999 * blockSize) / blockSize);
+  }
 
-    getMapY(){
-        return parseInt(this.y / blockSize);
-    }
+  changeAnimation() {
+    this.currentFrame =
+      this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+  }
 
-    getMapXRightSide() {
-        return parseInt((this.x + 0.9999 * blockSize)/blockSize);
-    }
-
-    getMapYRightSide() {
-        return parseInt((this.y + 0.9999 * blockSize)/blockSize);
-    }
-
-    changeAnimation() {
-        this.currentFrame = this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
-    }
-
-    draw() {
-        canvasCtx.save();
-        canvasCtx.translate(this.x + blockSize /2, this.y + blockSize /2);
-        canvasCtx.rotate((this.direction * 90 * Math.PI)/180);
-        canvasCtx.translate(-this.x - blockSize /2, -this.y - blockSize /2);
-        canvasCtx.drawImage(pacmanFrames,(this.currentFrame -1) * blockSize, 0, blockSize, blockSize, this.x, this.y, this.width, this.height);
-        canvasCtx.restore();
-    }
+  draw() {
+    canvasCtx.save();
+    canvasCtx.translate(this.x + blockSize / 2, this.y + blockSize / 2);
+    canvasCtx.rotate((this.direction * 90 * Math.PI) / 180);
+    canvasCtx.translate(-this.x - blockSize / 2, -this.y - blockSize / 2);
+    canvasCtx.drawImage(
+      pacmanFrames,
+      (this.currentFrame - 1) * blockSize,
+      0,
+      blockSize,
+      blockSize,
+      this.x,
+      this.y,
+      this.width,
+      this.height,
+    );
+    canvasCtx.restore();
+  }
 }
